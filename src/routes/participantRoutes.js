08b2a13de5e088f0d423ffc7fa93ebec123c15de
Your Name <viktoriia.i.sokolenko@gmail.com>
2025-04-07 21:39:12 -0500

    feat:endpoints for get, put, and delete requests for participants

--- src/routes/participantRoutes.js
+++ src/routes/participantRoutes.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const participantController = require('../controllers/participantController');
-const authMiddleware = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.get('/', participantController.getParticipants);
 router.get('/:participantid', participantController.getParticipantInfo);
 router.put('/:participantid', participantController.updateParticipant);
 router.delete('/:participantid', participantController.deleteParticipant);
-
 
 module.exports = router;
